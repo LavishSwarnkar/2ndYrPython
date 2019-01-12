721320cbdf4436cd6cdd727dd65540b407568853
Lavish Swarnkar <lavishswarnkar@gmail.com>
2019-01-12 14:19:13 +0530

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a number: "))
+if(x<0):
+    print("It is a negative number.")
+else:
+    print("It is a positive number.")
+
---
+++ L4.py
@@ -0,0 +1,6 @@
+x = int(input("Enter year: "))
+if(x%4==0):
+    print("It is a leap year.")
+else:
+    print("It is not a leap year.")
+
